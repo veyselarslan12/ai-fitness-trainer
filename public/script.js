5bd2e62c48f0248bc895adda39f0bae23bb027cb
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-11 18:33:07 -0500

    fake prompt created for testing purposes

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const height = "5'10\"" 
+const weight = ""
+const heightMessage = height ? `My height is ${height}`: ''
+const weightMessage = weight ? `My weight is ${weight}`: ''
+
+const fakePrompt = `You are my fitness trainer and i want you to a suggest fitness plan, ${heightMessage} ${weightMessage} `
+ 
