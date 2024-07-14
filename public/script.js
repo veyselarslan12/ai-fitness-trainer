cc7e044a252c7b7e2e68c7a984b092b7b063b9ec
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-13 23:06:50 -0500

    workout prompt

--- public/script.js
+++ public/script.js
@@ -8,14 +8,20 @@
 const form = document.querySelector('form')
 
 const name = document.getElementById('name').value
-const age = document.getElementById('age').value
-const gender = document.getElementById('gender').value
+const age = document.getElementById('age').value ? `I am ${age} years old.`: ''
+const gender = document.getElementById('gender').value // TODO: Get help from teacher
 const height = document.getElementById('height').value
 const weight = document.getElementById('weight').value
 const workoutTypes = document.getElementById('workoutTypes').value
-const activityLevel = document.getElementById('activityLevel').value
 const medicalConditions = document.getElementById('medicalConditions').value
 const dietaryPreferences = document.getElementById('dietaryPreferences').value
 const workoutFrequency = document.getElementById('workoutFrequency').value
 const workoutAvailability = document.getElementById('workoutAvailability').value
 const preferredLocation = document.getElementById('preferredLocation').value
+const result = document.getElementById('result')
+
+const medicalConditionsMessage = medicalConditions ? `My medical issue is ${medicalConditions}`: ''
+const dietaryPreferencesMessage = `My dietary preferences is ${dietaryPreferences}`
+const workoutFrequencyMessage = workoutFrequency ? `I would like to workout ${workoutFrequency}`: ''
+
+const workoutPrompt = `My name is ${name}.${age}.`
