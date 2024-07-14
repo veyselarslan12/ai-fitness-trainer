5ccec2d2019fb2c7e8e4da07798fff42f2855e89
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-13 22:20:51 -0500

    getting value from inputs in script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,21 @@
-const height = "5'10\"" 
-const weight = ""
-const heightMessage = height ? `My height is ${height}`: ''
-const weightMessage = weight ? `My weight is ${weight}`: ''
+// const height = "5'10\"" 
+// const weight = ""
+// const heightMessage = height ? `My height is ${height}`: ''
+// const weightMessage = weight ? `My weight is ${weight}`: ''
 
-const fakePrompt = `You are my fitness trainer and i want you to a suggest fitness plan, ${heightMessage} ${weightMessage} `
+// const fakePrompt = `You are my fitness trainer and i want you to a suggest fitness plan, ${heightMessage} ${weightMessage} `
  
+const form = document.querySelector('form')
+
+const name = document.getElementById('name').value
+const age = document.getElementById('age').value
+const gender = document.getElementById('gender').value
+const height = document.getElementById('height').value
+const weight = document.getElementById('weight').value
+const workoutTypes = document.getElementById('workoutTypes').value
+const activityLevel = document.getElementById('activityLevel').value
+const medicalConditions = document.getElementById('medicalConditions').value
+const dietaryPreferences = document.getElementById('dietaryPreferences').value
+const workoutFrequency = document.getElementById('workoutFrequency').value
+const workoutAvailability = document.getElementById('workoutAvailability').value
+const preferredLocation = document.getElementById('preferredLocation').value
